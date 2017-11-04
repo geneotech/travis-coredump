fb79cf10d867d07319208e017f7a7f2a7f560f8f
geneotech <unknown.unreleased@gmail.com>
2017-11-04 09:31:15 +0100

    test without cxx1z flag

--- might_crash.cpp
+++ might_crash.cpp
@@ -1,16 +1,11 @@
 #include <cstdlib>
 #include <string>
-#include <optional>
-#include <variant>
 
 int main(int argc, char** argv) {
-  std::optional<int> abc = argc;
-  std::variant<int, double> vvv = argc;
-
   if( argc > 1 && std::string(argv[1]) == "--crashplx" ) {
       // this will crash
       delete (int*)0xffffffff;    
   }
 
-  return 0 * (*abc) * std::get<int>(vvv); 
+  return 0; 
 }

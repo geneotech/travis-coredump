66bbda7864c721a0637c062765139c19af7b1575
geneotech <unknown.unreleased@gmail.com>
2017-11-04 08:45:49 +0100

    even more minimal

--- might_crash.cpp
+++ might_crash.cpp
@@ -1,8 +1,11 @@
 #include <cstdlib>
+#include <string>
 
-int main() {
-  if( getenv("CRASH_PLEASE") ) {
+int main(int argc, char** argv) {
+  if( argc > 1 && std::string(argv[1]) == "--crashplx" ) {
       // this will crash
       delete (int*)0xffffffff;    
-  } 
+  }
+
+  return 0; 
 }

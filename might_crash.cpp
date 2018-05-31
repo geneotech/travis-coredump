aa3181de79efe1b336ed94c1fce17e7e338845ef
Patryk Czachurski <patryk.czachurski@gmail.com>
2018-05-31 11:05:55 +0200

    Update might_crash.cpp

--- might_crash.cpp
+++ might_crash.cpp
@@ -2,8 +2,36 @@
 #include <string>
 #include <optional>
 #include <variant>
+#include <iostream>
+
+template <class S, class... Types>
+constexpr bool is_one_of_v = (... || std::is_same_v<S, Types>);
+
+template <class d>
+class a {
+    auto& get() {
+        return static_cast<d*>(this)->v;
+    }
+
+    const auto& get() const {
+        return static_cast<d*>(this)->v;
+    }
+public:
+    void f() {
+        std::cout << get() << std::endl;
+    }
+};
+
+struct b : a<b> {
+    int v = 42;
+};
 
 int main(int argc, char** argv) {
+  b bb;
+  bb.f();
+  static_assert(is_one_of_v<int, double, int>);
+  !static_assert(is_one_of_v<int, double>);
+  
   std::optional<int> abc = argc;
   std::variant<int, double> vvv = argc;
 
